9f740526aa408ba14ccac53349e14f2ec1784963
xubingde <xubingde@hotmail.com>
2025-08-12 08:21:29 +0800

    enum

--- src/icode.h
+++ src/icode.h
@@ -25,7 +25,7 @@ enum class endian {
     native = little
 #else
     little = __ORDER_LITTLE_ENDIAN__,
-    big = __ORDER_BIG_ENDIAN__,
+    big    = __ORDER_BIG_ENDIAN__,
     native = __BYTE_ORDER__
 #endif
 };
